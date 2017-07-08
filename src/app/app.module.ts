88cc5cd1d45a9f63c71dec5e364d0756b46190dd
Rahul Pandey <rahul.pandey@nagarro.com>
2017-07-08 17:18:33 +0530

    ng server communication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { HeroListComponent } from './toh/hero-list.component';
+import { InMemoryWebApiModule }     from 'angular-in-memory-web-api';
+import { HeroData }                 from './hero-data';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule,
+    InMemoryWebApiModule.forRoot(HeroData)
   ],
   providers: [],
   bootstrap: [AppComponent]
